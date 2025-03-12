df1a39696e5832cd2c5ba3be9fa40f1afc16fce7
Sanjana24kairamkonda <96222161+Sanjana24kairamkonda@users.noreply.github.com>
2025-03-12 15:07:22 +0530

    Update backend.tf

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "backend_storage1"  # The bucket you created
+    bucket = "my-unique-bucket-eng-artifact-453305-g4"  # The bucket you created
     prefix = "terraform/state"            # Prefix for state file in the bucket
   }
 }
