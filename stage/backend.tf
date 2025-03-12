920914289eb6930a351f83e8f0cf9b1a894e9b1a
Sanjana24kairamkonda <96222161+Sanjana24kairamkonda@users.noreply.github.com>
2025-03-12 15:08:52 +0530

    Update backend.tf

--- stage/backend.tf
+++ stage/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "backend_storage1"  # The bucket you created
+    bucket = "backend_stage"  # The bucket you created
     prefix = "terraform/state"            # Prefix for state file in the bucket
   }
 }
